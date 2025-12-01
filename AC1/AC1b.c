75bdc80ba35455c03667e4833e90c2300d94006c
ErynRev <erynsosocial@gmail.com>
2025-12-01 21:07:05 +0000

    modified:   AC1/AC1a 	modified:   AC1/AC1a.c 	new file:   AC1/AC1aOutput.txt 	new file:   AC1/AC1b 	new file:   AC1/AC1b.c 	new file:   AC1/AC1bOutput.txt 	new file:   AC1/natsAC1b 	new file:   AC1/natsAC1b.c

--- AC1/AC1b.c
+++ AC1/AC1b.c
@@ -2,23 +2,12 @@
 I am C code Dumbie so please freakin
 bear
 wif
-
-BLEH!!! (code just disappeared on me trynna member)
+ 
 
 What do i need to do?
 
-FILE command with pointer to fopen file to read
-Create int starting count at 0
-Create int defining  start
-Create int defining  the change in number,, deltnum
-Create char defining direction
-
-fscanf to scan for L and R assigning each negative and positive respectfully
-then subtract/add digits after that in same row to previous position
-loop of when going below 0 or above 99, it returns back down; ---- you can tell im dumb bc i forgot that 0-99 is 100 digits
-i.e., above 99 subtract 100, below 0 add 100
-
-Count how many times 0 comes up in sequence
+This is exact same but this time count each time it goes past 0 even if its not landing on 0
+baso just add to count each time pos (start) goes >99 or <0
 
 wish me luck
 */
@@ -33,10 +22,6 @@ int main(void) {
     char direction; // Define direction positive or negative
 
     fptr=fopen("./AC1/AC1aNums.txt", "r");  // fptr then opens the AC1aNums.txt file as "r"ead only
-    if (!fptr) {
-        printf("Error opening file.\n");
-        return 1;
-    }
 
     while (fscanf(fptr, "%c%d\n", &direction, &deltnum) == 2) { // %c refers to the character and will go to direction character 
         // %d gives number change
@@ -50,21 +35,24 @@ int main(void) {
 
         while (start > 99) {
             start -= 100; // take away 100
+            count += 1; // also add to count
         }
         
         while (start < 0) {
-            start += 100;
+            start += 100; // add 100
+            count += 1; // also add to count
         }
 
-        if (start == 0) {
+        /* if (start == 0) {
             count += 1;
         }
+        */
 
         printf("%c\t%d\t%d\t%d\n", direction, deltnum, start, count);
 
     }
 
-    printf("%d\n", count);
+    printf("%d \n", count);
 
 return 0;
 }
