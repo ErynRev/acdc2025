ddcad58d3025ed66dc77f07d864bd2627c7408b2
ErynRev <erynsosocial@gmail.com>
2025-12-02 17:01:30 +0000

    modified:   AC1/AC1a 	new file:   AC2/AC2Nums     	new file:   AC2/AC2a.c

---
+++ AC2/AC2a.c
@@ -0,0 +1,54 @@
+/*
+
+hellooo AGAIN
+its the fucken second day
+and i promised 
+
+What do i need to do?
+
+the file gives ranges of IDs,, but in these ranges if there are any repeating IDs, they are notorised as invalid
+
+i.e,
+in the range 222220-222225, there is one invalid ID in 222222 as it splits into 222,222 (i.e., 2 lots of 222)
+
+SOOOO i need to search the ranges given, (only EVEN amount of digits)
+
+therefore im gonna have to:
+-- Go up range 1 by 1 -- For loop
+-- Check each number to see if it is even number of digits -- 
+-- AND THEN Split the number down the middle
+-- THEN CHECK If the numbers on each side equal
+
+while scanning
+    scan for start int and end int;
+
+
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    /*FILE *fptr;
+    // int digits;
+    int start;
+    int end;
+    // int step;
+    // int count;
+    
+    fptr = fopen("./AC2/AC2Nums", "r");
+    if (!fptr) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    // while (fscanf(fptr, "%d-%d\n", &start, &end) == 2) {
+        
+        printf("%d\t and %d\t \n", start, end);
+    }
+
+*/
+return 0;
+
+}
+
