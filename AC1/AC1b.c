88880b962e82d324741245c3034042d17be4e857
ErynRev <erynsosocial@gmail.com>
2025-12-02 13:45:30 +0000

    modified:   AC1/AC1b 	modified:   AC1/AC1b.c

--- AC1/AC1b.c
+++ AC1/AC1b.c
@@ -16,44 +16,41 @@ wish me luck
 
 int main(void) {
     FILE *fptr; // This tells the FILE variable to point to fptr defined below
-    int count = -1;
+    int count = 0;
     int start = 50; // Defining the starting positon as 50
     int newpos; //defining position end
-    int oldpos; //def position old start
+    int oldpos=start; //def position old start
     int deltnum; // Define change in num (deltnum)
     char direction; // Define direction positive or negative
 
-    fptr=fopen("./AC1/AC1aNums.txt", "r");  // fptr then opens the AC1aNums.txt file as "r"ead only
+    fptr=fopen("./AC1/d1.txt", "r");  // fptr then opens the AC1aNums.txt file as "r"ead only
 
     while (fscanf(fptr, "%c%d\n", &direction, &deltnum) == 2) { // %c refers to the character and will go to direction character 
-        oldpos = start;
+    
         
         // %d gives number change
-        if (direction == 76) { // dec code for L = Left
+        if (direction == 'L') { // dec code for L = Left
             newpos = oldpos - deltnum; // subtract change from position
-            
-        }
-
-        else {  // else Right
-            newpos = oldpos + deltnum; // add change
+            while (newpos < 0) {
+                newpos += 100; // add 100
+                count ++; // also add to count
+            /*if (oldpos == 0) {
+                count ++;
+            }*/
         
-        }
-
-        while (newpos > 99) {
-            newpos -= 100; // take away 100
-            count ++; // also add to count
-        
-        }
-        
-        while (newpos < 0) {
-            newpos += 100; // add 100
-            count ++; // also add to count
-            if (oldpos == 0) {
-                count -=2;
             }
-        
+
         }
 
+        else if (direction == 'R') {  // else Right
+            newpos = oldpos + deltnum; // add change
+            while (newpos > 99) {
+                newpos -= 100; // take away 100
+                count ++; // also add to count
+            }
+        }
+     
+        
 
         printf("%c\t%d\t%d\t%d\n", direction, deltnum, newpos, count);
 
